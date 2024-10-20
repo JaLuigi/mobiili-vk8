f1c8324cb464b288d3835c3383d201cdf34ecf29
JaLuigi <t2luja00@students.oamk.fi>
2024-10-20 14:45:54 +0300

    vk8

---
+++ app/src/main/java/viewmodel/TodoViewModel.kt
@@ -0,0 +1,28 @@
+package viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TodoViewModel : ViewModel() {
+    var todos = mutableStateListOf<Todo>()
+        private set
+
+    init {
+        getTodosList()
+    }
+
+    private fun getTodosList() {
+        viewModelScope.launch {
+            try {
+                val todosApi = TodosApi.getInstance()
+                todos.clear()
+                todos.addAll(todosApi.getTodos())
+            } catch (e: Exception) {
+                Log.d("TODOVIEWMODEL", e.message.toString())
+            }
+        }
+    }
+}
